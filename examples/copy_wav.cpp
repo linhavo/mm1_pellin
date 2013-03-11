4668ca3b0252a3b5b8c6c61c7a668784423d18a5
Zdenek Travnicek <travnicek@iim.cz>
2013-03-11 16:27:47 +0100

    Added example copy_wav to simply copy a file from a WaveSource to WaveSink

--- examples/copy_wav.cpp
+++ examples/copy_wav.cpp
@@ -1,12 +1,12 @@
 /*
- * playback_high.cpp
+ * copy_wav.cpp
  *
  *  Created on: 11.3.2013
  *      Author: neneko
  */
 
 
-#include "iimavlib/AlsaSink.h"
+#include "iimavlib/WaveSink.h"
 #include "iimavlib/WaveSource.h"
 #include "iimavlib/Utils.h"
 #include <string>
@@ -18,20 +18,25 @@ int main(int argc, char** argv)
 	/* ******************************************************************
 	 *                Process command line parameters
 	 ****************************************************************** */
-	if (argc<2) {
-		logger[log_level::fatal] << "Not enough parameters. Specify the wave file please";
+	if (argc<3) {
+		logger[log_level::fatal] << "Not enough parameters. Specify the in and out wave files.";
 		return 1;
 	}
+
+
 	const std::string filename (argv[1]);
-	logger[log_level::debug] << "Loading file " << filename;
+	const std::string filename2 (argv[2]);
+	logger[log_level::debug] << "Copying " << filename << " -> " << filename2;
 
 	/* ******************************************************************
 	 *                Create and run the filter chain
 	 ****************************************************************** */
 
 	pAudioSink chain = filter_chain<WaveSource>(filename)
-						.add<AlsaSink>("sysdefault")
+						.add<WaveSink>(filename2)
 						.sink();
 	chain->run();
 
 }
+
+
