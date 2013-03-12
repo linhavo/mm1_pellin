50655d46eeeeabd219ccdffcc14d5f64fdd10593
Zdenek Travnicek <travnicek@iim.cz>
2013-03-12 17:28:56 +0100

    Added possibility to record a wav file in playthrough_high

--- examples/playthrough_high.cpp
+++ examples/playthrough_high.cpp
@@ -5,21 +5,38 @@
  *      Author: neneko
  */
 
-#include "iimavlib/AlsaSource.h"
-#include "iimavlib/AlsaSink.h"
+#include "iimavlib.h"
 #include "iimavlib/WaveSink.h"
 #include "iimavlib/filters/NullFilter.h"
 #include "iimavlib/filters/SimpleEchoFilter.h"
+#include "iimavlib/Utils.h"
 
 int main(int argc, char** argv)
 {
 	using namespace iimavlib;
+
+	/* ******************************************************************
+	 *                Process command line parameters
+	 ****************************************************************** */
+
+	audio_id_t device_in = PlatformDevice::default_device();
+	audio_id_t device_out = PlatformDevice::default_device();
+	std::string out_file;
+	if (argc>1) device_in = simple_cast<audio_id_t>(argv[1]);
+	if (argc>2) device_out = simple_cast<audio_id_t>(argv[2]);
+	if (argc>3) out_file = simple_cast<std::string>(argv[3]);
+	/* ******************************************************************
+	 *                Create and run the filter chain
+	 ****************************************************************** */
+
+
 	audio_params_t params(sampling_rate_t::rate_48kHz, sampling_format_t::format_16bit_signed, 1);
-	pAudioSink chain = filter_chain<AlsaSource>(params,"hw:2,0")
+	auto filters = filter_chain<PlatformSource>(params,device_in)
 						.add<NullFilter>()
-						.add<SimpleEchoFilter>(0.1)
-						.add<AlsaSink>("sysdefault")
-//						.add<WaveSink>("out.wav")
+						.add<SimpleEchoFilter>(0.2);
+	if (!out_file.empty()) filters = filters
+						.add<WaveSink>(out_file);
+	auto chain = filters.add<PlatformSink>(device_out)
 						.sink();
 	chain->set_buffers(4,2048);
 	chain->run();
