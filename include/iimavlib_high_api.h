523f5e15f0b2ec4137d94cbbc93c45d32fb882f1
Zdenek Travnicek <travnicek@iim.cz>
2013-03-20 08:14:28 +0100

    Added header for high_api

---
+++ include/iimavlib_high_api.h
@@ -0,0 +1,16 @@
+/**
+ * @file 	AlsaDevice.h
+ *
+ * @date 	20.3.2013
+ * @author 	Zdenek Travnicek <travnicek@iim.cz>
+ * @copyright GNU Public License 3.0
+ *
+ * This file defines the library's high level API
+ */
+
+#ifndef HIGH_API_H_
+#define HIGH_API_H_
+#include "iimavlib/AudioPlatform.h"
+
+
+#endif /* HIGH_API_H_ */
