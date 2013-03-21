7cfaeebd5e0cfbfb30ebb5ee76e810e7f1bf3fe0
Roman Berka <berka@fel.cvut.cz>
2013-03-21 18:13:44 +0100

    test changed to transform 2
    Signed-off-by: Zdenek Travnicek <travnicek@iim.cz>

--- examples/sdl_test.cpp
+++ examples/sdl_test.cpp
@@ -17,28 +17,33 @@
 #include <algorithm>
 #include <iostream>
 ///*
+#include <cmath>
 
-const int xMax=80;
-const int yMax=80;
-const int cx=xMax/2;
-const int cy=yMax/2;
+const int xMax=500;
+const int yMax=500;
+
 
 int round(double x) {
+#ifdef SYSTEM_LINUX
+	return std::round(x);
+#else
 	return (int) (x+0.5);
+#endif
 }
 
-template<typename T> void move(T&d, T&s, int dx, int dy) {
+//posunuti
+template<typename T> void shiftbmp(T&d, T&s, int dx, int dy) {
 	int xn,yn,x,y;
 	for(y=0;y<yMax;y++) {
 		for(x=0;x<xMax;x++) {
 			xn=(x-dx+xMax)%xMax;
 			yn=(y-dy+yMax)%yMax;
 			d[yn*xMax+xn]=s[y*xMax+x];
-			//d[yn*xMax+xn].r=(d[y*xMax+x].r+1)%255;
 		}
 	}
 }
 
+//rotace okolo leveho horniho rohu
 template<typename T> void rot0(T&d, T&s, double deg) {
 	const double PI=3.1415926;
 	double omega=2*PI*deg/360.0;
@@ -46,115 +51,158 @@ template<typename T> void rot0(T&d, T&s, double deg) {
 	int xn,yn,x,y;
 	
 	for(y=0;y<yMax;y++) {
-		x=0;
 		for(x=0;x<xMax;x++) {
 			xn=round(x*cosT+y*sinT);
 			yn=round(y*cosT-x*sinT);
 			
 			if(xn>=0 && xn<xMax && yn>=0 && yn<yMax) 
 				d[y*xMax+x]=s[yn*xMax+xn];
-			else {
-				d[y*xMax+x].r=0;d[y*xMax+x].g=0;d[y*xMax+x].b=0;
-			}
+			//else 
+				//d[y*xMax+x].r=0;d[y*xMax+x].g=0;d[y*xMax+x].b=0;
+		
 		}
 	}
-	d[0].r=100;	d[0].g=0;	d[0].b=0;
-	s[0].r=0;	s[0].g=0;	s[0].b=100;
 }
 
+//rotace okolo stredu
 template<typename T> void rot(T&d, T&s, double deg) {
-	const double PI=3.1415926;
+	const double PI=3.14159265;
 	double omega=2*PI*deg/360.0;
 	double cosT=cos(omega), sinT=sin(omega);
 	int xn,yn,x,y;
+	const int cx=xMax/2;
+	const int cy=yMax/2;
+
 	for(y=0;y<yMax;y++) {
-		xn=round((0-cx)*cosT+(y-cy)*sinT+cx);
-		yn=round((y-cy)*cosT-(0-cx)*sinT+cy);
+		//xn=round(  (0-cx)*cosT+(y-cy)*sinT+cx   );//(-cx + x)*cosT+(y-cy)*sinT+cx 
+		//yn=round(  (y-cy)*cosT-(0-cx)*sinT+cy     ); //(-cy+y)*cosT-(-cx+x)*sinT+cy
 		for(x=0;x<xMax;x++) {
+			xn=round(  (x-cx)*cosT+(y-cy)*sinT+cx   );//(-cx + x)*cosT+(y-cy)*sinT+cx 
+			yn=round(  (y-cy)*cosT-(x-cx)*sinT+cy     ); //(-cy+y)*cosT-(-cx+x)*sinT+cy
 			if(xn>=0 && xn<xMax && yn>=0 && yn<yMax) 
 				d[y*xMax+x]=s[yn*xMax+xn];
-			xn+=round(cosT);
-			yn-=round(sinT);
+			else {
+				d[y*xMax+x].r=0;d[y*xMax+x].g=0;d[y*xMax+x].b=0;}
+			//xn+=round(cosT);
+			//yn-=round(sinT);
 		}
 	}
 }
 //*/
 
+//rotace okolo bodu cx,cy
+template<typename T> void rotc(T&d, T&s, double deg, int cx=0, int cy=0) {
+	const double PI=3.14159265;
+	double omega=2*PI*deg/360.0;
+	double cosT=cos(omega), sinT=sin(omega);
+	int xn,yn,x,y;
+
+	for(y=0;y<yMax;y++) {
+		//xn=round(  (0-cx)*cosT+(y-cy)*sinT+cx   );//(-cx + x)*cosT+(y-cy)*sinT+cx 
+		//yn=round(  (y-cy)*cosT-(0-cx)*sinT+cy     ); //(-cy+y)*cosT-(-cx+x)*sinT+cy
+		for(x=0;x<xMax;x++) {
+			xn=round(  (x-cx)*cosT+(y-cy)*sinT+cx   );//(-cx + x)*cosT+(y-cy)*sinT+cx 
+			yn=round(  (y-cy)*cosT-(x-cx)*sinT+cy     ); //(-cy+y)*cosT-(-cx+x)*sinT+cy
+			if(xn>=0 && xn<xMax && yn>=0 && yn<yMax) 
+				d[y*xMax+x]=s[yn*xMax+xn];
+			else {
+				d[y*xMax+x].r=0;d[y*xMax+x].g=0;d[y*xMax+x].b=0;}
+			//xn+=round(cosT);
+			//yn-=round(sinT);
+		}
+	}
+}
+
+//rotace sub-mapy x1,y1,x2,y2 okolo bodu cx,cy
+template<typename T> void rotcsub(T&d, T&s, double deg, int cx=0, int cy=0,int x1=0, int y1=0, int x2=xMax, int y2=yMax) {
+	const double PI=3.14159265;
+	double omega=2*PI*deg/360.0;
+	double cosT=cos(omega), sinT=sin(omega);
+	int xn,yn,x,y;
+
+	if(x1>=0 && x1<=xMax &&
+		y1>=0 && y1<=yMax &&
+		x2>=0 && x2<=xMax &&
+		y2>=0 && y2<=yMax) {
+
+		for(y=y1;y<y2;y++) {
+			//xn=round(  (0-cx)*cosT+(y-cy)*sinT+cx   );//(-cx + x)*cosT+(y-cy)*sinT+cx 
+			//yn=round(  (y-cy)*cosT-(0-cx)*sinT+cy     ); //(-cy+y)*cosT-(-cx+x)*sinT+cy
+			for(x=x1;x<x2;x++) {
+				xn=round(  (x-cx)*cosT+(y-cy)*sinT+cx   );//(-cx + x)*cosT+(y-cy)*sinT+cx 
+				yn=round(  (y-cy)*cosT-(x-cx)*sinT+cy     ); //(-cy+y)*cosT-(-cx+x)*sinT+cy
+				if(xn>=0 && xn<xMax && yn>=0 && yn<yMax) 
+					d[y*xMax+x]=s[yn*xMax+xn];
+				else {
+					d[y*xMax+x].r=0;d[y*xMax+x].g=0;d[y*xMax+x].b=0;}
+				//xn+=round(cosT);
+				//yn-=round(sinT);
+			}
+		}
+	}
+}
+
+//prohozeni obsahu dvou vektoru
 template<typename T>void swapt(T &p1,T &p2) {
-	T tmp=p1;
-	p1=p2;
-	p2=tmp;
+	T tmp=std::move(p1);
+	p1=std::move(p2);
+	p2=std::move(tmp);
 }
 
 int main()
 {
 	iimavlib::SDLDevice sdl(xMax,yMax,"ROT!");
 	iimavlib::SDLDevice::data_type data(xMax*yMax),rx(xMax*yMax);
-	iimavlib::SDLDevice::data_type *d=&data,*s=&rx;
 	
 	int i=0,j=0;
 	bool swtch=false;
 	bool swtch2=false;
-	//data[i].b=0;
-	//rx[1]=data[1];
-	const double angle=3; //degrees
-
-	/*for(i=0;i<xMax;i++) {
-		if(j%10==0) swtch2=!swtch2;
-		for(j=0;j<yMax;j++) {
-			if(i%10==0) swtch=!swtch;
-			
-			if(swtch || swtch2) data[j*xMax+i].r=data[j*xMax+i].g=data[j*xMax+i].b=0;
-			else data[j*xMax+i].r=data[j*xMax+i].g=data[j*xMax+i].b=255;
-
-		}
-		
-	}*/
 	
+	int rectx1=xMax/4,recty1=yMax/4,rectx2=xMax-xMax/4,recty2=yMax-yMax/4;
+	int cx=xMax/2, cy=yMax/2;
+
+	//inicializace bitmapy
 	std::for_each(data.begin(),data.end(),[&](iimavlib::RGB&rgb){
 			
+		//inicializace - cerne pozadi
 		rgb.r=rgb.g=rgb.b=0;
+		//zapnout/vypnout inkoust kazdych 10 sloupcu
 		if(i%10==0) 
 			swtch=!swtch;
-		
-		if(swtch || swtch2)
+		//pokud je zapnuty inkoust a jsem v kreslici sub-mape kreslim bilou barvou
+		if((swtch || swtch2)  && i>=rectx1 && i < rectx2 && j>=recty1 && j<recty2)
 			rgb.r=rgb.g=rgb.b=255;
 		
+		//poskocit na pixel napravo, kdyz uz jsem na konci skoci to automaticky na zacatek
 		i=(i+1)%xMax;
 		
+		//odradkovani pred zacatkem radku
 		if(i==0) {
 			j=(j+1)%yMax;
+			//zapnout/vypnout inkoust kazdych 10 radek
 			if(j%10==0) swtch2=!swtch2;
 		}
 		});
+
 	//iimavlib::logger[iimavlib::log_level::info] << "i: " << i;
+	const double angle=6; //degrees
+
+	//nastartovani noveho vlakna
 	sdl.start();
+	//hlavni vykreslovaci smycka
 	while(sdl.update(data)) {
 		int i=0,j=0;
-		//for (int n=0;n<1000000;n++);
-		
-		/*std::for_each(data.begin(),data.end(),[&](iimavlib::RGB&rgb){
-			
-			rgb.r=(rgb.r+1)%255;
-			rgb.g=(rgb.g+1)%255;
-			rgb.b=(rgb.b+1)%255;
-
-			i=(i+1)%xMax;
-			if(i==0) j=(j+1)%yMax;
-		});*/
-		//swapt<iimavlib::SDLDevice::data_type*>(d,s);
-		//std::swap(d,s);
-		//rot0<iimavlib::SDLDevice::data_type>(*d,*s,angle);
+	
 		std::swap(data,rx);
-		//move<iimavlib::SDLDevice::data_type>(*d,*s,1,1);
-		move<iimavlib::SDLDevice::data_type>(data,rx,1,1);
-		//std::swap(data,rx);
-		//std::cout << "iterace\n";
 
+		//rotcsub<iimavlib::SDLDevice::data_type>(data,rx,angle,cx,cy,rectx1,recty1,rectx2,recty2);
+		rotcsub<iimavlib::SDLDevice::data_type>(data,rx,angle,cx,cy);
+		//shiftbmp<iimavlib::SDLDevice::data_type>(data,rx,1,1);
+		
 #ifdef SYSTEM_LINUX
 		usleep(1000);
 #else
-		Sleep(100);
+		Sleep(10);
 #endif
 	}
 	sdl.stop();
