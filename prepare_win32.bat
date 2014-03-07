d332d9b2cb9c75bd1c57219b9e5de7b1c0fe815c
Zdenek Travnicek <travnicek@iim.cz>
2014-03-07 11:25:41 +0100

    Fixed bug in prepare_win* scripts.

--- prepare_win32.bat
+++ prepare_win32.bat
@@ -1,4 +1,4 @@
 mkdir build
-copy libs/x86/SDL.dll build
+copy lib\x86\SDL.dll build
 cd build
 cmake.exe .. -DSDL_INCLUDE_DIR=../include/SDL -DSDL_LIBRARY_TEMP=../lib/x86/SDL -DCMAKE_BUILD_TYPE=Release -G"Visual Studio 11"
