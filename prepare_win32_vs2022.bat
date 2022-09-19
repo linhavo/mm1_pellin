8fe055d691eea6b85bea79034ca1138f055fa8fe
Ondřej Slabý <kron258@gmail.com>
2022-09-19 14:09:45 +0200

    Add prepare_* scripts for VS2022

---
+++ prepare_win32_vs2022.bat
@@ -0,0 +1,4 @@
+mkdir build
+copy lib\x86\SDL.dll build
+cd build
+cmake.exe .. -DSDL_INCLUDE_DIR=../include/SDL -DSDL_LIBRARY_TEMP=../lib/x86/SDL -DCMAKE_BUILD_TYPE=Release -G"Visual Studio 17 2022"
