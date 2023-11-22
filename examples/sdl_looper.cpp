c8636a47839ccbe977b2f60b78e496c406c0f929
irontach <irontach@gmail.com>
2023-11-22 17:10:31 +0100

    :sparkles: sequencer merged with empty 2 looper

--- examples/sdl_looper.cpp
+++ examples/sdl_looper.cpp
@@ -6,6 +6,12 @@
  * @copyright GNU Public License 3.0
  *
  */
+#include "iimavlib/AudioFFT.h"
+#include "iimavlib/AudioTypes.h"
+#include "SDL/SDL_video.h"
+#include "iimavlib/WaveSource.h"
+#include "iimavlib/filters/SineMultiply.h"
+
 
 #include <iimavlib/SDLDevice.h>
 #include <iimavlib/AudioFilter.h>
@@ -15,6 +21,10 @@
 #include <iimavlib/keys.h>
 #include <mutex>
 #include <cmath>
+
+#include <algorithm>
+#include <atomic>
+#include <functional>
 
 
 using namespace iimavlib;
@@ -157,6 +167,20 @@ public:
 		AudioFilter(child), instruments_(instruments), steps_(steps), last_step_(-1), loop_length_(loop_length), time_(0.0f), data_(width, height)
 	{
 		sequence_.resize(instruments * steps, false);
+		time_ = 50.0f;
+		const audio_params_t& params = get_params();
+		cache_size_ = static_cast<size_t>(time_ * convert_rate_to_int(params.rate));
+		cache_size_ = static_cast<size_t>(pow(2, ceil(log2(cache_size_))) * 2);
+		logger[log_level::info] << "Cache size: " << cache_size_;
+		sample_cache_.resize(cache_size_);
+		barwidth = 40;
+		x_count = 0;
+		thread_ = std::thread(std::bind(&Control::execute_thread, this));
+		time_elapsed = 0.000f;
+		whole = (1800.000f / cache_size_);
+		const auto max_int16_value = std::numeric_limits<int16_t>::max();
+
+		magic_constant = 1.0f / 16384.0f / max_int16_value;
 
 		start();
 	}
@@ -172,11 +196,109 @@ private:
 	int last_step_;
 	double loop_length_;
 	double time_;
+	std::thread thread_;
+	std::mutex mutex_;
+	std::vector<audio_sample_t> sample_cache_;
+	int width_;
+	int x_count;
+	int height_;
+	int barwidth;
+	std::atomic<bool> end_;
+	std::atomic<bool> changed_;
+	size_t last_sample_;
+	size_t cache_size_;
+	std::vector<complexarray_t<float>> coefficient_array_entire;
+	float time_elapsed;
+	float whole;
+	float magic_constant;
+
+
+	AudioFFT<float> fft;
 
 	/// Video data
 	iimavlib::video_buffer_t data_;
 	/// Sequence data
 	std::vector<bool> sequence_;
+
+	void execute_thread() {
+		logger[log_level::debug] << "Drawing thread started";
+		while (!end_) {
+			if (changed_) {
+				draw_wave();
+			}
+			if (!blit(data_)) {
+				logger[log_level::debug] << "Drawing thread finishing";
+				end_ = true;
+			}
+		}
+		auto surface = SDL_GetVideoSurface();
+		SDL_SaveBMP(surface, "screenshot.bmp");
+	}
+
+
+	void update_cache(const audio_buffer_t& buffer) {
+		std::unique_lock<std::mutex> lock(mutex_);
+		const audio_sample_t* src = &buffer.data[0];
+		size_t src_remaining = buffer.valid_samples;
+
+		while (src_remaining) {
+			const size_t to_copy = std::min(src_remaining, sample_cache_.size() - last_sample_);
+			std::copy_n(src, to_copy, &sample_cache_[0] + last_sample_);
+			last_sample_ += to_copy;
+			if (last_sample_ >= sample_cache_.size()) last_sample_ = 0;
+			src_remaining -= to_copy;
+			//coefficient_array_entire[ind] = fft.FFT1D(sample_cache_.begin(), sample_cache_.end());
+		}
+		changed_.store(true);
+	}
+
+	void draw_wave() {
+		// Max value for int16_t
+
+		changed_.store(false);
+
+		// Array for the coefficients from FFT
+		complexarray_t<float> coefficient_array;
+		{
+			std::unique_lock<std::mutex> lock(mutex_);
+			coefficient_array = fft.FFT1D(sample_cache_.begin(), sample_cache_.end());
+			time_elapsed += time_;
+
+
+		}
+
+		const auto unique_coefficients = (coefficient_array.size() + 1) / 2;
+
+
+		for (int y = 0; y < height_; ++y) {
+			const size_t coefficient_number = y * unique_coefficients / height_;
+
+			// Get value for the coefficient
+			auto coefficient = std::abs(coefficient_array[coefficient_number]) * magic_constant;
+			coefficient *= 100;
+
+			auto yycol = static_cast<int>(255 * (coefficient));
+
+			std::vector<int> colr = { 1, 0 , 0 };
+			colr[0] = int(yycol);
+			colr[2] = int(50 * y / height_);
+			for (auto i = 0; i < 3; i++) {
+				colr[i] += int(yycol ^ 2);
+				if (colr[i] > 255) {
+					colr[i] = 255;
+				}
+			}
+			draw_bars(int(time_elapsed * whole * 100) % width_, y, colr);
+		}
+
+	}
+
+
+	void draw_bars(int x, int y, std::vector<int> colr) {
+		rectangle_t rectangle = intersection(data_.size, rectangle_t(x, y, barwidth / 20, height_ - y));
+		iimavlib::draw_rectangle(data_, rectangle_t(rectangle.x, rectangle.y, rectangle.width, 3), rgb_t(colr[0], colr[1], colr[2]));
+
+	}
 
 	void draw_sequence()
 	{
@@ -224,7 +346,9 @@ private:
 		// Update our loop time (and loop it if appropriate)
 		time_ += buffer.valid_samples * 1.0 / convert_rate_to_int(buffer.params.rate);
 		if (time_ > loop_length_) time_ -= loop_length_;
-		draw_sequence();
+		//draw_sequence();
+		update_cache(buffer);
+
 
 		// Get our current step and check if we want any generators enabled
 		int cur_step = current_step();
