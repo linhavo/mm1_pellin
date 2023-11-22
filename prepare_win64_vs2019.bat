826d916f0f0f97d496e1e39a9b4d7eda6eb6f9d0
Vojta Linha <linhavo2@fel.cvut.cz>
2023-11-22 16:50:55 +0100

    :sparkles: Create looper file

--- prepare_win64_vs2019.bat
+++
@@ -1,4 +0,0 @@
-mkdir build
-copy lib\x64\SDL.dll build
-cd build
-cmake.exe .. -DSDL_INCLUDE_DIR=../include/SDL -DSDL_LIBRARY_TEMP=../lib/x64/SDL.lib -DCMAKE_BUILD_TYPE=Release -G"Visual Studio 16 2019" -A x64
